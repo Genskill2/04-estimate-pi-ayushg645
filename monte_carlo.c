8ce24d58864e0e8e5403e37f3e6126dd1e5bb558
ayushg645 <85107691+ayushg645@users.noreply.github.com>
2021-06-04 16:03:12 +0530

    Implemented monte-carlo

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,7 +3,32 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
+float mc pi(int itr) 
+ { int in=0, tot=0;
+
+ float x,y,pi, dist;
+
+
+
+for(int i=1;i<=itr;i++)
+
+{x=frandom(); 
+y=frandom();
+
+dist= sqrt( (x*x)+(y*y) );
+
+if( dist <= 1 )
+
+{in++;}
+
+tot++;
+
+}
+
+pi= (4*in)/ (float) tot;
+
+ return pi;
+ }
 
 float frandom() {
   long int q = random();
